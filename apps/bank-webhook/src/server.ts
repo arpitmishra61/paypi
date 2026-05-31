a0c04097a9948f69a568abbf8e2d2626fb4f34ba
Arpit Mishra <37569669+arpitmishra61@users.noreply.github.com>
2026-05-31 22:45:42 +0530

    github action test

--- apps/bank-webhook/src/server.ts
+++ apps/bank-webhook/src/server.ts
@@ -60,5 +60,5 @@ app.post("/web-hook", async (req, res) => {
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Webhook Started on port ", PORT);
+  console.log("Webhook Started on the port ", PORT);
 });
